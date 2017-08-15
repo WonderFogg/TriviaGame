76eec05005dc4f127fb94f5fc436062641469c68
Joe Fogg <GJF610@gmail.com>
2017-08-14 22:14:52 -0400

    Qs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,45 +6,7 @@ const submitButton = document.getElementById('submit');
 
 
 // follow layout sitepoint need one to build and one for results 
-function buildQuiz(){}
-
-function showResults(){}
-
-// display quiz right away
-buildQuiz();
-
-// on submit, show results
-submitButton.addEventListener('click', showResults);
-
-const myQuestions = [
-{
-	question: "Q111111111111111?",
-	answers: {
-		a: "AAAAAAAAAA",
-		b: "BBBBBBBBBB",
-		
-	},
-	correctAnswer: "b"
-},     
-{   question: "Q2222222222222222?",
-    answers: {
-      a: "aaaaaaaaaa",
-      b: "bbbbbbbbbbb",
-      
-    },
-    correctAnswer: ""
-  },
-  {
-    question: "q3333333333333333?",
-    answers: {
-      a: "aaaaaaaaaa",
-      b: "bbbbbbbbbbb",
-     
-    },
-    correctAnswer: ""
-  }
-
-  ];
+f
 
 // MORE QUESTIONS // 
 // MORE QUESTIONS // 
@@ -64,43 +26,3 @@ const myQuestions = [
 // MORE QUESTIONS // 
 
 
-
-function buildQuiz(){
-  // we'll need a place to store the HTML output
-  const output = [];
-
-  // for each question...
-  myQuestions.forEach(
-    (currentQuestion, questionNumber) => {
-
-      // we'll want to store the list of answer choices
-      const answers = [];
-
-      // and for each available answer...
-      for(letter in currentQuestion.answers){
-
-        // ...add an HTML radio button
-        answers.push(
-          `<label>
-            <input type="radio" name="question${questionNumber}" value="${letter}">
-            ${letter} :
-            ${currentQuestion.answers[letter]}
-          </label>`
-        );
-      }
-
-      // add this question and its answers to the output
-      output.push(
-        `<div class="question"> ${currentQuestion.question} </div>
-        <div class="answers"> ${answers.join('')} </div>`
-      );
-    }
-  );
-
-  // finally combine our output list into one string of HTML and put it on the page
-  quizContainer.innerHTML = output.join('');
-}
-
-
-
-]
